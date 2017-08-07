8cb464bff00429e14a36b2af607777522d346f50
Pawel Szczepkowski <pawel_szczepkowski@satlan.pl>
2017-08-07 10:33:34 +0200

    [test] andd category with products dao test

--- src/main/test/java/pl/allegier/controller/dao/category/CategoryDaoTest.java
+++ src/main/test/java/pl/allegier/controller/dao/category/CategoryDaoTest.java
@@ -1,11 +1,18 @@
 package pl.allegier.controller.dao.category;
 
+import org.junit.After;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pl.allegier.controller.dao.AbstractDaoTest;
 import pl.allegier.controller.dao.Dao;
 import pl.allegier.controller.dao.DaoTest;
+import pl.allegier.controller.dao.product.ProductDao;
 import pl.allegier.model.Category;
+import pl.allegier.model.Product;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @Component
 public class CategoryDaoTest  extends AbstractDaoTest<Category,String> implements DaoTest<Category,String>{
@@ -13,9 +20,38 @@ public class CategoryDaoTest  extends AbstractDaoTest<Category,String> implement
     @Autowired
     private CategoryDao categoryDao;
 
+    @Autowired
+    private ProductDao productDao;
+
     @Override
     public Dao<Category, String> getRepository() {
         return categoryDao;
+    }
+
+    @After
+    public void cleanUp()
+    {
+        productDao.removeAll();
+        categoryDao.removeAll();
+    }
+
+    @Test
+    public void saveCategoryWithProducts()
+    {
+        Product product = createProduct();
+        Category category= createEntity();
+        product.setCategory(category);
+        categoryDao.save(category);
+        Product save = productDao.save(product);
+        Product persisted = productDao.findById(save.getId());
+        assertThat(persisted.getTitle(), equalTo(save.getTitle()));
+    }
+
+    public Product createProduct()
+    {
+        Product product = new Product();
+        product.setTitle("1234");
+        return product;
     }
 
     @Override
